47f13cc580c1464584479e09345c249c80d51ec9
github-action-benchmark <github@users.noreply.github.com>
2023-03-09 05:01:31 +0000

    add Benchmark.NET Benchmark (benchmarkdotnet) benchmark result for 33fd45d4155f4c8375b30d2829e29541db059c61

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678338089763,
+  "lastUpdate": 1678338091611,
   "repoUrl": "https://github.com/hcdotnet/fiara-fna-benchmarks",
   "entries": {
     "Benchmark.NET Benchmark": [
@@ -23,6 +23,78 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/hcdotnet/fiara-fna-benchmarks/commit/33fd45d4155f4c8375b30d2829e29541db059c61"
         },
         "date": 1678338088696,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2ConstructorBenchmark.FnaVector2ConstructorInt32MultiCtor",
+            "value": 533.5206105158879,
+            "unit": "ns",
+            "range": "± 3.49189155061282"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2ConstructorBenchmark.SysVector2ConstructorInt32MultiCtor",
+            "value": 688.5497369264301,
+            "unit": "ns",
+            "range": "± 15.25119865423268"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2ConstructorBenchmark.FnaVector2ConstructorInt32SingleCtor",
+            "value": 532.9070772443499,
+            "unit": "ns",
+            "range": "± 3.5118964524334295"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2ConstructorBenchmark.SysVector2ConstructorInt32SingleCtor",
+            "value": 463.40696261479303,
+            "unit": "ns",
+            "range": "± 4.168265800222806"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2ConstructorBenchmark.FnaVector2ConstructorSingleMultiCtor",
+            "value": 507.85268374851773,
+            "unit": "ns",
+            "range": "± 0.7590965449495383"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2ConstructorBenchmark.SysVector2ConstructorSingleMultiCtor",
+            "value": 563.2215772356305,
+            "unit": "ns",
+            "range": "± 3.6090143839710143"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2ConstructorBenchmark.FnaVector2ConstructorSingleSingleCtor",
+            "value": 512.3412322998047,
+            "unit": "ns",
+            "range": "± 5.490869884014722"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2ConstructorBenchmark.SysVector2ConstructorSingleSingleCtor",
+            "value": 457.0040559768677,
+            "unit": "ns",
+            "range": "± 2.069029562256593"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "xxlennygamerxx@gmail.com",
+            "name": "Tomat",
+            "username": "steviegt6"
+          },
+          "committer": {
+            "email": "xxlennygamerxx@gmail.com",
+            "name": "Tomat",
+            "username": "steviegt6"
+          },
+          "distinct": true,
+          "id": "33fd45d4155f4c8375b30d2829e29541db059c61",
+          "message": "oh my god I forgor to run it",
+          "timestamp": "2023-03-08T20:54:00-08:00",
+          "tree_id": "933078d0eb4c3dbcc017c2ea7a93544ee22a4bcb",
+          "url": "https://github.com/hcdotnet/fiara-fna-benchmarks/commit/33fd45d4155f4c8375b30d2829e29541db059c61"
+        },
+        "date": 1678338090889,
         "tool": "benchmarkdotnet",
         "benches": [
           {
