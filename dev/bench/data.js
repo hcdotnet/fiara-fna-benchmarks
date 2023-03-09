019372272556b1d5f7ec3c0f5556f73c5b76c470
github-action-benchmark <github@users.noreply.github.com>
2023-03-09 05:30:19 +0000

    add Benchmark.NET Benchmark (benchmarkdotnet) benchmark result for 1b7e278bbb86bf027515cf869b23c83b1edc8fe6

---
+++ dev/bench/data.js
@@ -0,0 +1,176 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1678339819229,
+  "repoUrl": "https://github.com/hcdotnet/fiara-fna-benchmarks",
+  "entries": {
+    "Benchmark.NET Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "xxlennygamerxx@gmail.com",
+            "name": "Tomat",
+            "username": "steviegt6"
+          },
+          "committer": {
+            "email": "xxlennygamerxx@gmail.com",
+            "name": "Tomat",
+            "username": "steviegt6"
+          },
+          "distinct": true,
+          "id": "1b7e278bbb86bf027515cf869b23c83b1edc8fe6",
+          "message": "Oops, submodule.",
+          "timestamp": "2023-03-08T21:20:31-08:00",
+          "tree_id": "253dfce146963d09f9495eddb7fef3b8190ec633",
+          "url": "https://github.com/hcdotnet/fiara-fna-benchmarks/commit/1b7e278bbb86bf027515cf869b23c83b1edc8fe6"
+        },
+        "date": 1678339818064,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2ConstructorBenchmark.FnaVector2ConstructorInt32MultiCtor",
+            "value": 1038.3186072135459,
+            "unit": "ns",
+            "range": "± 41.56956261407758"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2ConstructorBenchmark.SysVector2ConstructorInt32MultiCtor",
+            "value": 1741.3085679372152,
+            "unit": "ns",
+            "range": "± 27.932129183136624"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2ConstructorBenchmark.FnaVector2ConstructorInt32SingleCtor",
+            "value": 987.3630716182568,
+            "unit": "ns",
+            "range": "± 26.87079220512728"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2ConstructorBenchmark.SysVector2ConstructorInt32SingleCtor",
+            "value": 1031.5441753967948,
+            "unit": "ns",
+            "range": "± 25.753479646948087"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2ConstructorBenchmark.FnaVector2ConstructorSingleMultiCtor",
+            "value": 948.62619155248,
+            "unit": "ns",
+            "range": "± 13.57293339078224"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2ConstructorBenchmark.SysVector2ConstructorSingleMultiCtor",
+            "value": 1738.4337096744114,
+            "unit": "ns",
+            "range": "± 36.08818352192298"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2ConstructorBenchmark.FnaVector2ConstructorSingleSingleCtor",
+            "value": 1039.9761245727539,
+            "unit": "ns",
+            "range": "± 13.7737385327599"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2ConstructorBenchmark.SysVector2ConstructorSingleSingleCtor",
+            "value": 1041.4010880788167,
+            "unit": "ns",
+            "range": "± 23.375224254668616"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2DivideBenchmark.FnaVector2Divide",
+            "value": 6417.950986480713,
+            "unit": "ns",
+            "range": "± 89.99303780528643"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2DivideBenchmark.SysVector2Divide",
+            "value": 1710.9002969264984,
+            "unit": "ns",
+            "range": "± 32.49505169902937"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2DivideBenchmark.FnaVector2DivideNoInline",
+            "value": 20760.085499354773,
+            "unit": "ns",
+            "range": "± 214.46271922639986"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2DivideBenchmark.SysVector2DivideNoInline",
+            "value": 2125.9009281921385,
+            "unit": "ns",
+            "range": "± 56.82224590653002"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2SubtractBenchmark.FnaVector2Subtract",
+            "value": 6948.61905328851,
+            "unit": "ns",
+            "range": "± 149.70472669193455"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2SubtractBenchmark.SysVector2Subtract",
+            "value": 1019.0939413070679,
+            "unit": "ns",
+            "range": "± 16.493726156347588"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2SubtractBenchmark.FnaVector2SubtractNoInline",
+            "value": 18576.51159362793,
+            "unit": "ns",
+            "range": "± 218.20330100153043"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2SubtractBenchmark.SysVector2SubtractNoInline",
+            "value": 2037.8414869944254,
+            "unit": "ns",
+            "range": "± 32.17110113789795"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2MultiplyBenchmark.FnaVector2Multiply",
+            "value": 7389.480155690511,
+            "unit": "ns",
+            "range": "± 58.06858765487397"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2MultiplyBenchmark.SysVector2Multiply",
+            "value": 1065.5552690596808,
+            "unit": "ns",
+            "range": "± 24.59764561453473"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2MultiplyBenchmark.FnaVector2MultiplyNoInline",
+            "value": 18741.13558898926,
+            "unit": "ns",
+            "range": "± 751.048317000005"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2MultiplyBenchmark.SysVector2MultiplyNoInline",
+            "value": 2124.621290567759,
+            "unit": "ns",
+            "range": "± 70.74684711253256"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2AddBenchmark.FnaVector2Add",
+            "value": 7019.537241872152,
+            "unit": "ns",
+            "range": "± 85.4260061091735"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2AddBenchmark.SysVector2Add",
+            "value": 1027.7872714996338,
+            "unit": "ns",
+            "range": "± 11.063346042433329"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2AddBenchmark.FnaVector2AddNoInline",
+            "value": 18716.86911714994,
+            "unit": "ns",
+            "range": "± 128.4253439385805"
+          },
+          {
+            "name": "FIARA.Benchmarks.Substitutions.Vector2AddBenchmark.SysVector2AddNoInline",
+            "value": 2255.647806358337,
+            "unit": "ns",
+            "range": "± 51.030928634596556"
+          }
+        ]
+      }
+    ]
+  }
+}
